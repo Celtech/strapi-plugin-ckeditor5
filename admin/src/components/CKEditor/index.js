80112f8040329259659839927ea1dc85bf817aea
Tim Hinz <tim@chargeover.com>
2022-04-25 13:37:12 -0500

    chore: convert to markdown plugin

--- admin/src/components/CKEditor/index.js
+++ admin/src/components/CKEditor/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "ckeditor5-build-strapi-wysiwyg";
+import ClassicEditor from "@chargeover/ckeditor5-markdown-build";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
